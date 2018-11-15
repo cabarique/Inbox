bd863b51cec06516b30c58e7423ec69464405ad7
Luis Cabarique <cabarique.luis@gmail.com>
2018-11-15 18:48:12 -0500

    Initial Commit

---
+++ Inbox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Inbox
+//
+//  Created by Luis Cabarique on 11/15/18.
+//  Copyright © 2018 cabarique inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
