a4d10f2273161bbf0095b6e39a21396e99497c33
Luis Cabarique <cabarique.luis@gmail.com>
2018-11-15 21:22:47 -0500

    add API

---
+++ Inbox/API/InboxAPI.swift
@@ -0,0 +1,92 @@
+//
+//  InboxAPI.swift
+//  Inbox
+//
+//  Created by Luis Cabarique on 11/15/18.
+//  Copyright © 2018 cabarique inc. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+var inboxAPI = MoyaProvider<inboxEndPoints>(plugins: [CachePolicyPlugin()])
+private let kURL = "https://jsonplaceholder.typicode.com/"
+
+enum inboxEndPoints {
+    case posts
+    case post(Int)
+    case comments(Int)
+    case user(Int)
+}
+
+extension inboxEndPoints: TargetType {
+    var baseURL: URL {
+        return URL(string: kURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .posts:
+            return "posts"
+        case .post(let id):
+            return "posts/\(id)"
+        case .comments:
+            return "comments"
+        case .user:
+            return "users"
+        }
+    }
+    
+    var method: Moya.Method {
+        return .get
+    }
+    
+    var sampleData: Data {
+        switch self {
+        default:
+            return "".data(using: String.Encoding.utf8)!
+        }
+        
+    }
+    
+    var task: Task {
+        switch self {
+        case .posts,
+             .post:
+            return .requestParameters(parameters: [:], encoding: URLEncoding.default)
+        case .comments(let id):
+            return .requestParameters(parameters: ["postId": id], encoding: URLEncoding.default)
+        case .user(let userId):
+            return .requestParameters(parameters: ["userId": userId], encoding: URLEncoding.default)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return nil
+    }
+    
+    
+}
+
+
+protocol CachePolicyGettable {
+    var cachePolicy: URLRequest.CachePolicy { get }
+}
+
+final class CachePolicyPlugin: PluginType {
+    public func prepare(_ request: URLRequest, target: TargetType) -> URLRequest {
+        if let cachePolicyGettable = target as? CachePolicyGettable {
+            var mutableRequest = request
+            mutableRequest.cachePolicy = cachePolicyGettable.cachePolicy
+            return mutableRequest
+        }
+        
+        return request
+    }
+}
+
+extension inboxEndPoints: CachePolicyGettable {
+    var cachePolicy: URLRequest.CachePolicy {
+        return .returnCacheDataElseLoad
+    }
+}
